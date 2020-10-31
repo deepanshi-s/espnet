182d510961140e33113fb7a29633a755ac0e344e
Deepanshi <44083363+deepanshi-s@users.noreply.github.com>
2020-10-31 21:46:12 +0530

    Update xvec_extractation.py

--- xvec_extractation.py
+++ xvec_extractation.py
@@ -4,11 +4,11 @@ import numpy as np
 import kaldiio
 import os
 
-for filename in os.listdir('exp/xvector_nnet_1a/xvectors_train_clean_100'):
+for filename in os.listdir('espnet/egs/libritts/tts1/exp/xvector_nnet_1a/xvectors_train_clean_100'):
   if filename.endswith('.ark'):
-    f = kaldiio.load_ark(os.path.join('exp/xvector_nnet_1a/xvectors_train_clean_100', filename)) # open in readonly mode
+    f = kaldiio.load_ark(os.path.join('espnet/egs/libritts/tts1/exp/xvector_nnet_1a/xvectors_train_clean_100', filename)) # open in readonly mode
     for key, numpy_array in f:
       print(filename,key)
-      path = 'libritts/xvector/'+key+'.txt'
+      path = 'xvector/'+key+'.txt'
       print(path)
       np.savetxt(path, numpy_array)
